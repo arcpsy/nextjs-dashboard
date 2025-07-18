e0570a6b16e0a42f9c7059f3fab2404aa8aa7abd
Carlo-Laynes <carloallanlaynes@gmail.com>
2025-07-19 05:25:01 +0800

    feat: centralize page titles using metadata template in root layout

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from "zod";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { neon } from "@neondatabase/serverless";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 
 const sql = neon(process.env.POSTGRES_URL!);
 
@@ -116,4 +118,23 @@ export async function deleteInvoice(id: string) {
    // Unreachable code block
    await sql`DELETE FROM invoices WHERE id = ${id}`;
    revalidatePath("/dashboard/invoices");
+}
+
+export async function authenticate(
+   prevState: string | undefined,
+   formData: FormData
+) {
+   try {
+      await signIn("credentials", formData);
+   } catch (error) {
+      if (error instanceof AuthError) {
+         switch (error.type) {
+            case "CredentialsSignin":
+               return "Invalid credentials.";
+            default:
+               return "Something went wrong.";
+         }
+      }
+      throw error;
+   }
 }
